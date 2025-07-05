845ebfb7c35e5d09ee6695e4bfc0a67dcc6bd0f1
asrul650 <tioasrul1234@email.com>
2025-07-05 16:00:12 +0700

    db

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -6,7 +6,7 @@
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root'); 
+define('DB_PASSWORD', ''); 
 define('DB_NAME', 'project_psi_db'); // <<< GANTI NAMA DATABASE JIKA PERLU
 
 // Create a new database connection
