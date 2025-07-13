6d9b2df1cc1117dabc4406627770b94f0cc56188
maritsataqy <maritsataqy@gmail.com>
2025-07-13 13:44:07 +0700

    ..

--- User/homepage.php
+++ User/homepage.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id_user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mobile Legends Guide - Home User</title>
+    <title>Mobile Legends Guider - Home User</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/herocard.css">
     <!--<link rel="stylesheet" href="../css/homepage.css">-->
